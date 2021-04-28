7dbba97ccd3c6da67714d1d34d5fb071b13f71b6
aetvictoria <aetvictoria@yahoo.com>
2021-04-28 17:46:19 +0800

    Json Reader Fixed; Multiple Errors due to changed SDK constraint

--- lib/userlogs_screen.dart
+++ lib/userlogs_screen.dart
@@ -23,56 +23,111 @@ class _UserLogsScreenState extends State<UserLogsScreen> {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {//TODO: Fix "type 'List<dynamic>' is not a unique subtype of type 'Map<String,dynamic>'"
     return Scaffold(
       appBar: AppBar(title: Text('User Logs'), centerTitle: true),
       //body: new UserLogsTable(), // Create table logs
       body: FutureBuilder<Entry>(
-        future: futureEntry,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Text(snapshot.data.department_id);
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
+            future: futureEntry,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.departmentName);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
 
-          // By default, show a loading spinner.
-          return CircularProgressIndicator();
-        },
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
       ),
       drawer: new DefaultDrawer(),
     );
   }
 }
 
-Future<Entry> fetchEntry() async {
+List<Entry> parseEntry(String responseBody) { //converts response to List<String>
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Entry>((json) => Entry.fromJson(json)).toList();
+}
+
+Future<List<Entry>> fetchEntry(http.Client client) async {
   final response =
-      await http.get(Uri.https('ais.amti.com.ph', '/OpenApi/departments'));
+      await client .get(Uri.parse('https://ais.amti.com.ph/OpenApi/departments'));
+
+return parseEntry(response.body);
+
+  // if (response.statusCode == 200) {
+  //   //if server access successful:
+  //   return Entry.fromJson(jsonDecode(response.body)); //parse
+  // } else {
+  //   //if server access unsuccessful:
+  //   throw Exception('Failed to load album'); //throw exception
+  // }
+}
+
+Future<Entry> fetchEntry() async {
+  final response = await http.get(Uri.https('ais.amti.com.ph', '/OpenApi/departments'));
 
   if (response.statusCode == 200) {
-    //if server access successful:
-    return Entry.fromJson(jsonDecode(response.body)); //parse
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Entry.fromJson(jsonDecode(response.body));
   } else {
-    //if server access unsuccessful:
-    throw Exception('Failed to load album'); //throw exception
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
   }
 }
 
-class Entry {
-  final String department_id;
-  final String department_name;
+class Entry { // Class for Json Entries
+  String id;
+  String departmentName;
 
-  Entry({@required this.department_id, @required this.department_name});
+  Entry({this.id, this.departmentName});
 
   factory Entry.fromJson(Map<String, dynamic> json) {
-    return Entry(
-      department_id: json['id'],
-      department_name: json['department_name']
+    return new Entry(
+      id: json['id'].toString(),
+      departmentName: json['department_name']
+    );
+  }
 
-    
+  // Map<String, dynamic> toJson() {
+  //   final Map<String, dynamic> data = new Map<String, dynamic>();
+  //   data['id'] = this.id;
+  //   data['department_name'] = this.departmentName;
+  //   return data;
+  // }
+}
+
+class EntriesList {
+  List<Entry> entries;
+
+    EntriesList({
+      this.entries
+    });
+
+    factory EntriesList.fromJson(List<dynamic> parsedJson) {
+
+    List<Entry> entries = new List<Entry>();
+
+    return new EntriesList(
+       entries: entries,
     );
   }
 }
+
+// factory EntryList.fromJson(List<dynamic> parsedJson){
+//   List<Entry> entries = new List<Entry>();
+
+//   entries = parsedJson.map((i)=>Entry.fromJson(i)).toList();
+
+
+//   return new Entry(
+//     entries: entries,
+//   );
+// } 
 
 class UserLogsTable extends StatelessWidget {
   // should be stateful if adding queries to table
